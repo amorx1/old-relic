ec8c551a582b4e655f061922fdf737a92ef9cb51
A Mor <akshay.morx@gmail.com>
2024-03-06 18:47:19 +1300

    timeseries

--- client/src/main.rs
+++ client/src/main.rs
@@ -31,32 +31,39 @@ async fn main() -> Result<()> {
         .api_key(api_key)
         .http_client(Client::builder());
 
-    let selected= client
-        .query::<ApplicationResult>(
-            "SELECT (appName, entityGuid) FROM Transaction WHERE appName LIKE 'fre-address-api-v2-prod'",
-        )
-        .await
-        .and_then(|a| a.first().map(Application::from))
-        .ok_or(anyhow!("No applications found!"))?;
+    // let selected= client
+    //     .query::<ApplicationResult>(
+    //         "SELECT (appName, entityGuid) FROM Transaction WHERE appName LIKE 'fre-address-api-v2-prod'",
+    //     )
+    //     .await
+    //     .and_then(|a| a.first().map(Application::from))
+    //     .ok_or(anyhow!("No applications found!"))?;
 
-    let trace = client
-        .query::<TraceResult>(format!(
-            "SELECT traceId FROM Transaction WHERE entity.Guid = {} SINCE 1 minute ago",
-            selected.entity_guid
-        ))
-        .await
-        .and_then(|t| t.first().map(Trace::from))
-        .ok_or(anyhow!("No traces found!"))?;
+    // let trace = client
+    //     .query::<TraceResult>(format!(
+    //         "SELECT traceId FROM Transaction WHERE entity.Guid = {} SINCE 1 minute ago",
+    //         selected.entity_guid
+    //     ))
+    //     .await
+    //     .and_then(|t| t.first().map(Trace::from))
+    //     .ok_or(anyhow!("No traces found!"))?;
 
-    dbg!(&trace);
-
-    let data_point = client.query::<TimeseriesResult>(
-        "SELECT sum(newrelic.goldenmetrics.infra.awsapigatewayresourcewithmetrics.requests) AS 'Requests' FROM Metric WHERE entity.guid in ('MjU0MDc5MnxJTkZSQXxOQXw4MDE0OTk0OTg4MDIzNTAxOTQ0', 'MjU0MDc5MnxJTkZSQXxOQXw4Njc2NDEwODc3ODY4NDI2Mzcz', 'MjU0MDc5MnxJTkZSQXxOQXwtODA4OTQxNjQyMzkzODMwODg2NQ', 'MjU0MDc5MnxJTkZSQXxOQXwtMzMzMDQwNzA1ODI3MDQwODE5MA', 'MjU0MDc5MnxJTkZSQXxOQXw0NjY5MzQ3MTY5NTU5NDUyODc2', 'MjU0MDc5MnxJTkZSQXxOQXwxNTA2ODc2MTg0MjI0NTQyNjU3') FACET entity.name LIMIT MAX TIMESERIES SINCE 5 minutes ago UNTIL now")
+    let data = client.query::<TimeseriesResult>(
+        "SELECT sum(newrelic.goldenmetrics.infra.awsapigatewayresourcewithmetrics.requests) AS 'Requests' FROM Metric WHERE entity.guid in ('MjU0MDc5MnxJTkZSQXxOQXw4MDE0OTk0OTg4MDIzNTAxOTQ0', 'MjU0MDc5MnxJTkZSQXxOQXw4Njc2NDEwODc3ODY4NDI2Mzcz', 'MjU0MDc5MnxJTkZSQXxOQXwtODA4OTQxNjQyMzkzODMwODg2NQ', 'MjU0MDc5MnxJTkZSQXxOQXwtMzMzMDQwNzA1ODI3MDQwODE5MA', 'MjU0MDc5MnxJTkZSQXxOQXw0NjY5MzQ3MTY5NTU5NDUyODc2', 'MjU0MDc5MnxJTkZSQXxOQXwxNTA2ODc2MTg0MjI0NTQyNjU3') FACET entity.name LIMIT MAX TIMESERIES SINCE 7 days ago UNTIL now")
         .await
-        .and_then(|p| p.first().map(Timeseries::from))
         .ok_or(anyhow!("No timeseries data found!"))?;
 
-    dbg!(&data_point);
+    let mut dataset: Vec<_> = vec![];
+
+    data.into_iter()
+        .map(Timeseries::from)
+        .map(|t| t.plot())
+        .for_each(|(start, end)| {
+            dataset.push(start);
+            dataset.push(end);
+        });
+
+    dbg!(&dataset);
 
     Ok(())
 }
